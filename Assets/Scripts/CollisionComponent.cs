8acc6bef4fb393b803b565708f7b06fa1795f86c
Frank Su <jhihwei.su@gmail.com>
2018-05-05 22:26:21 +0800

    296L4

---
+++ Assets/Scripts/CollisionComponent.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionComponent : MonoBehaviour {
+
+	public AudioClip collisionClip;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if(other.relativeVelocity.magnitude > 10){
+			GetComponent<AudioSource>().PlayOneShot(collisionClip);
+		}
+	}
+}
---
+++ Assets/Scripts/ThrowCom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowCom : MonoBehaviour {
+
+	public GameObject BallPrefab;
+	public float speed = 15.0f;
+
+	void Update () {
+		if(Input.GetButtonUp("Fire1")){
+			GameObject newObj = Instantiate(BallPrefab);
+			newObj.GetComponent<Rigidbody>().position = transform.position;
+			newObj.GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+			Destroy(newObj, 3f);
+		}
+	}
+}
